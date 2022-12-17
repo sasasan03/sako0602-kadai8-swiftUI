d686c9d39c1e9ce9f130658b286c1c66ed25981d
sasasan03 <sako.hiro62@gmail.com>
2022-12-17 23:04:24 +0900

    課題８提出

---
+++ kadai8-sako0602-SwiftUI/ExperimentView.swift
@@ -0,0 +1,20 @@
+//
+//  ExperimentView.swift
+//  kadai8-sako0602-SwiftUI
+//
+//  Created by sako0602 on 2022/12/17.
+//
+
+import SwiftUI
+
+struct ExperimentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExperimentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExperimentView()
+    }
+}
